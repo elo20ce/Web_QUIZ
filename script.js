f6aad6993c23870f0dae039f8def63d122ebbe3c
Mawulolo TONA <tonamawulolo@gmail.com>
2024-01-09 23:04:06 +0000

    couleur des réponses du Quiz .....

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const startBtn = document.querySelector('.start-btn');
 const popupInfo = document.querySelector('.popup-info');
 const exitBtn = document.querySelector('.exit-btn');
 const main = document.querySelector('.main');
+const continueBtn = document.querySelector('.continue-btn');
+const quizSection = document.querySelector('.quiz-section');
+const quizBox = document.querySelector('.quiz-box');
 
 startBtn.onclick = () => {
     popupInfo.classList.add('active');
@@ -11,4 +14,58 @@ startBtn.onclick = () => {
 exitBtn.onclick = () => {
     popupInfo.classList.remove('active');
     main.classList.remove('active');
+}
+
+continueBtn.onclick = () => {
+    quizSection.classList.add('active');
+    popupInfo.classList.remove('active');
+    main.classList.remove('active');
+    quizBox.classList.add('active');
+
+    showQuestions(0);
+    questionCounter(1);
+}
+
+let questionCount = 0;
+let questionNumb = 1;
+const nextBtn = document.querySelector('.next-btn');
+nextBtn.onclick = () => {
+    if (questionCount < questions.length -1) {
+    questionCount++;
+    showQuestions(questionCount);
+    questionNumb++;
+    questionCounter(questionNumb);
+    }
+    else {
+        console.log('Question Completed');
+    }
+}
+const optionList = document.querySelector('.option-list');
+// obtenir des questions et des options du tableau
+function showQuestions(index) {
+    const questionText = document.querySelector('.question-text');
+    questionText.textContent = `${questions[index].numb}. ${questions[index].question}`;
+    let optionTag = `<div class="option"><span>${questions[index].options[0]}</span></div>
+        <div class="option"><span>${questions[index].options[1]}</span></div>
+        <div class="option"><span>${questions[index].options[2]}</span></div>
+        <div class="option"><span>${questions[index].options[3]}</span></div>`;
+    optionList.innerHTML = optionTag;
+    const option = document.querySelectorAll('.option');
+    for (let i = 0; i < option.length; i++) {
+        option[i].setAttribute('onclick', 'optionSelected(this)');
+    }
+}
+function optionSelected(answer) {
+    let userAnswer = answer.textContent;
+    let correctAnswer = questions[questionCount].answer;
+    if (userAnswer == correctAnswer) {
+        answer.classList.add('correct');
+    }
+    else {
+        answer.classList.add('incorrect');
+    }
+}
+function questionCounter(index) {
+    const questionTotal = document.querySelector('.question-total');
+    questionTotal.textContent = `${index} of ${questions.length} Questions`;
 }
